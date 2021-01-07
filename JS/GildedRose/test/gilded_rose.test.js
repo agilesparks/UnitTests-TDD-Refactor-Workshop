ee598311a3b780117fad78db27b09cb79c949833
yaki koren <yaki@agilesparks.com>
2021-01-07 17:50:25 +0200

    nodeJS approvals

--- JS/GildedRose/test/gilded_rose.test.js
+++ JS/GildedRose/test/gilded_rose.test.js
@@ -1,9 +1,18 @@
 const {Shop, Item} = require("../src/gilded_rose");
+var approvals = require('approvals');
+approvals.configure({
+     reporters: ['beyondcompare']
+       });
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("fixme");
+  });
+
+  it('should be able to use Approvals', function () {
+    var data = "Hello World 2 !";
+    approvals.verify('.','example_test',data);  // or this.verifyAsJSON(data)
   });
 });
