87ab468a7ff5e2e28a9e7cba76f97288b368658e
yaki koren <yaki@agilesparks.com>
2021-01-24 10:10:47 +0200

    approvals reporter, location

--- python/gildedrose/approvals.py
+++ python/gildedrose/approvals.py
@@ -1,11 +1,15 @@
 from approvaltests.approvals import verify
 from approvaltests.reporters.generic_diff_reporter import GenericDiffReporter
+from approvaltests.reporters.generic_diff_reporter_factory import GenericDiffReporterFactory
 import unittest
 
 class GildedRoseTest(unittest.TestCase):
+    def setUp(self):
+        self.myReporter = GenericDiffReporter(('Custom', "C:/Program Files (x86)/WinMerge/WinMergeU.exe"))
+
     def test_approval(self):
         result = "foobar"
-        verify(result, GenericDiffReporter(('Custom', 'C:/Program Files/Beyond Compare 4/bcompare.exe')))
+        verify(result, self.myReporter )
 
 if __name__ == '__main__':
     unittest.main()
--- python/gildedrose/approvalsCombination.py
+++ python/gildedrose/approvalsCombination.py
@@ -4,11 +4,14 @@ from approvaltests.reporters.generic_diff_reporter import GenericDiffReporter
 import unittest
 
 class GildedRoseTest(unittest.TestCase):
+    def setUp(self):
+        self.myReporter = GenericDiffReporter(('Custom', "C:/Program Files (x86)/WinMerge/WinMergeU.exe"))
+
     def multiply(self,x,y):
         return x*y
 
     def test_approval(self):
-        verify_all_combinations(self.multiply,[[1,2,3],[10,20,30]], reporter=GenericDiffReporter(('Custom', 'C:/Program Files/Beyond Compare 4/bcompare.exe')))
+        verify_all_combinations(self.multiply,[[1,2,3],[10,20,30]], reporter=self.myReporter)
 
 
 if __name__ == '__main__':
