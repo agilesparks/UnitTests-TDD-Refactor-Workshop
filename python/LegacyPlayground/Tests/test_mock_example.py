63fa15abab4b53e3447f5edd0cf52517324c78f5
yaki koren <yaki@agilesparks.com>
2021-01-24 18:56:01 +0200

    python mock

---
+++ python/LegacyPlayground/Tests/test_mock_example.py
@@ -0,0 +1,16 @@
+﻿import unittest
+from unittest.mock import Mock
+from Production.UnbelievableComplexClass import UnbelivableComplexClass
+from Production.UnInstantiableClass import UnInstantiableClass
+from Production.InterestingClass import InterestingClass
+
+
+class Sanity(unittest.TestCase):
+    def test_method(self):
+        myInteresting = InterestingClass()
+        self.assertEqual(2676, myInteresting.longMethod(5))
+        mock=Mock()
+        mock.return_value=-200
+        myInteresting.getSomethingFromDB=mock
+        self.assertEqual(1801, myInteresting.longMethod(5))
+
