a9597e48953018899a5db44accc23fb9f421138d
yaki koren <yaki@agilesparks.com>
2021-02-01 09:26:58 +0200

    more mock examples

--- csharp/LegacyPlayground/Tests/MoqExample.cs
+++ csharp/LegacyPlayground/Tests/MoqExample.cs
@@ -12,9 +12,14 @@ namespace Legacycodeplaygroundtests
             myInt = initialInt;
         }
 
-        virtual public int getMyInt()
+        public virtual int GetMyInt()
         {
             return myInt;
+        }
+
+        public virtual int CallMe(int x)
+        {
+            return x;
         }
     }
 
@@ -26,19 +31,34 @@ namespace Legacycodeplaygroundtests
         public void MoqTest()
         {
             var mock = new Mock<ToBeMocked>(3);
-            mock.Setup(x => x.getMyInt()).Returns(3);
+            mock.Setup(x => x.GetMyInt()).Returns(3);
 
-            Assert.AreEqual(3, mock.Object.getMyInt());
+            Assert.AreEqual(3, mock.Object.GetMyInt());
         }
         
         [TestMethod]
         public void MoqTestSequence()
         {
             var mock = new Mock<ToBeMocked>(3);
-            mock.SetupSequence(x => x.getMyInt()).Returns(3).Returns(2);
+            mock.SetupSequence(x => x.GetMyInt()).Returns(3).Returns(2);
 
-            Assert.AreEqual(3, mock.Object.getMyInt());
-            Assert.AreEqual(2, mock.Object.getMyInt());
+            Assert.AreEqual(3, mock.Object.GetMyInt());
+            Assert.AreEqual(2, mock.Object.GetMyInt());
+        }
+
+        [TestMethod]
+        public void MoqCalled()
+        {
+            var mock = new Mock<ToBeMocked>(4);
+            int param = 0;
+           
+            mock.Setup(x => x.CallMe(It.IsAny<int>()))
+                .Callback<int>(r => param = r);
+
+            mock.Object.CallMe(17);
+            Assert.AreEqual(17,param);
+            mock.Verify(x => x.CallMe(17), Times.Exactly(1));
+
         }
     }
 }
